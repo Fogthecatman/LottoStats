7f79b45c704d344bb8287e9ef210e601d9e82733
Fogthecatman <fogthecatman@gmail.com>
2016-01-12 22:12:44 -0600

    Fourth Commit starting the project with jade files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp  = require('gulp');
-var jade  = require('gulp-jade');
-var sass  = require('gulp-sass');
+var gulp        = require('gulp');
+var jade        = require('gulp-jade');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync');
 
 /* JADE Tasks */
 /* This task will seek all the jade files inside _jadefiles */
@@ -11,15 +12,15 @@ gulp.task('jade', function(){
 		console.log(err);
 		this.emit('end');
 	})
-	.pipe(gulp.dest('_includes'));
+	.pipe(gulp.dest('_includes/pages'));
 });
 
 /* SASS Tasks */
 /* This task will seek all scss files ancompile them */
 gulp.task('sass', function(){
-   return gulp.src('')                                // find source folder
-          .pipe(sass.sync().on('error' sass.logError))
-          .pipe(gulp.dest(''));                       //find destination folder
+   return gulp.src('stylesheets/_main.scss')                                // find source folder
+          .pipe(sass.sync().on('error', sass.logError))
+          .pipe(gulp.dest('_includes/css/styles.css'));                       //find destination folder
 });
 
 /*BR/**/
@@ -31,6 +32,14 @@ gulp.task('browser-sync', ['jade'], function() {
 /*This task reloads the web page*/
 gulp.task('jade-reload', ['jade'], browserSync.reload);
 
-gulp.task('css-reload', function() {
-	return gulp.src('_includes')        //This line should have destination CSS file
+gulp.task('css-reload', function () {
+	return gulp.src('_includes/css/styles.css')
+	.pipe(browserSync.stream({match: '**/*.css'}));
 });
+
+gulp.task('watch', function () {
+    gulp.watch('_jadefiles/**/*.jade', ['jade', 'jade-reload']);
+		gulp.watch('_includes/css/styles.css', ['css-reload']);
+});
+
+gulp.task('default', ['watch', 'jade', 'browser-sync', 'sass']);
